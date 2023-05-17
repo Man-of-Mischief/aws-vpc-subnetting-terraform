7ff35403953a21af4ebe27263b0985028e848f4d
Nidhin B <106511145+Man-of-Mischief@users.noreply.github.com>
2023-05-17 12:13:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,41 @@
+########### Variables
+
+variable "ami"{
+	description = "ami id"
+	type = string
+	default = "ami-08333bccc35d71140"
+}
+
+variable "type"{
+	description = "instance type"
+	type = string
+	default = "t2.micro"
+}
+
+variable "project"{
+	description = "project tag value"
+	type = string
+	default = "uber"
+}
+
+variable "env" {
+	description = "env tag value"
+	type = string
+	default = "prod"
+}
+
+variable "access_key"{
+        default = "abcd"
+}
+
+variable "secret_key"{
+        default = "efgh"
+}
+
+variable "az" {
+	default = "us-east-2"
+}
+
+variable "vpcidr"{
+	default = "172.17.0.0/16"
+}
